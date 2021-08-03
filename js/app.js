eb8613fd363e30c9248381a41916756da0c69bdd
Joaquín <gomezjoaquin@protonmail.com>
2021-08-03 16:17:50 -0300

    Validate the Form.

--- js/app.js
+++ js/app.js
@@ -15,22 +15,33 @@ function buscarClima(e) {
     const pais = document.querySelector('#pais').value;
 
     //Consultar la API
-    if(ciudad === '' || pais === ''){
+    if (ciudad === '' || pais === '') {
         //Error para
         mostrarError('Ambos campos son obligatorios.');
 
         return;
     }
-    
+
 }
 
-function mostrarError(mensaje){
-    console.log(mensaje);
+function mostrarError(mensaje) {
+    const alerta = document.querySelector('.bg-red-100');
 
-    //Crear una alerta.
-    const alerta = document.createElement('div');
+    if (!alerta) {
+        //Crear una alerta.
+        const alerta = document.createElement('div');
 
-    alerta.classList.add('bg-red-100', 'border-red-400', 'text-red-700', 'px-4', 'py-3', 'rounded', 'max-w-md', 'mx-auto', 'mt-6', 'text-center' );
+        alerta.classList.add('bg-red-100', 'border-red-400', 'text-red-700', 'px-4', 'py-3', 'rounded', 'max-w-md', 'mx-auto', 'mt-6', 'text-center');
 
-    alerta.innerHTML = 
+        alerta.innerHTML = `
+    <strong class ="font-blof">Error!</strong>
+    <span class ="block">${mensaje}</span>
+    `;
+        container.appendChild(alerta);
+    }
+
+    setTimeout(() => {
+        alerta.remove();
+    }, 5000);
+
 }
