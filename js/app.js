165a6aa0702d2af7935590feb93cf0bffc149b29
Joaquín <gomezjoaquin@protonmail.com>
2021-08-03 16:02:20 -0300

    Update app.js

--- js/app.js
+++ js/app.js
@@ -9,5 +9,28 @@ window.addEventListener('load', () => {
 
 function buscarClima(e) {
     e.preventDefault();
-    console.log('buscando clima');
+
+    //Validar
+    const ciudad = document.querySelector('#ciudad').value;
+    const pais = document.querySelector('#pais').value;
+
+    //Consultar la API
+    if(ciudad === '' || pais === ''){
+        //Error para
+        mostrarError('Ambos campos son obligatorios.');
+
+        return;
+    }
+    
+}
+
+function mostrarError(mensaje){
+    console.log(mensaje);
+
+    //Crear una alerta.
+    const alerta = document.createElement('div');
+
+    alerta.classList.add('bg-red-100', 'border-red-400', 'text-red-700', 'px-4', 'py-3', 'rounded', 'max-w-md', 'mx-auto', 'mt-6', 'text-center' );
+
+    alerta.innerHTML = 
 }
